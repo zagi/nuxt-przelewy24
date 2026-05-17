3e8b36adb1746f3b7fcaf75bfa09a0e19ac03740
Michał Zagalski <mlzagalski@gmail.com>
2026-05-17 05:46:46 +0200

    feat: add module options and runtime config

--- src/module.ts
+++ src/module.ts
@@ -1,19 +1,60 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import { addImportsDir, addServerHandler, createResolver, defineNuxtModule } from '@nuxt/kit'
+import { defu } from 'defu'
+import type { P24Environment } from 'przelewy24-ts-sdk'
 
-// Module options TypeScript interface definition
-export interface ModuleOptions {}
+export interface ModuleOptions {
+  merchantId: number
+  posId?: number
+  apiKey: string
+  crcKey: string
+  environment?: P24Environment
+  webhookPath?: string
+}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'my-module',
-    configKey: 'myModule',
+    name: 'nuxt-przelewy24',
+    configKey: 'p24',
+    compatibility: { nuxt: '>=3.13.0' },
   },
-  // Default configuration options of the Nuxt module
-  defaults: {},
-  setup(_options, _nuxt) {
-    const resolver = createResolver(import.meta.url)
+  defaults: {
+    merchantId: 0,
+    apiKey: '',
+    crcKey: '',
+    environment: 'sandbox',
+    webhookPath: '/api/p24/webhook',
+  },
+  setup(options, nuxt) {
+    const { resolve } = createResolver(import.meta.url)
 
-    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
+    nuxt.options.runtimeConfig.p24 = defu(nuxt.options.runtimeConfig.p24, {
+      merchantId: options.merchantId,
+      posId: options.posId ?? options.merchantId,
+      apiKey: options.apiKey,
+      crcKey: options.crcKey,
+      environment: options.environment,
+      webhookPath: options.webhookPath,
+    })
+
+    addImportsDir(resolve('./runtime/server'))
+
+    addServerHandler({
+      route: options.webhookPath!,
+      method: 'post',
+      handler: resolve('./runtime/server/webhook'),
+    })
   },
 })
+
+declare module '@nuxt/schema' {
+  interface RuntimeConfig {
+    p24: {
+      merchantId: number
+      posId: number
+      apiKey: string
+      crcKey: string
+      environment: P24Environment
+      webhookPath: string
+    }
+  }
+}
